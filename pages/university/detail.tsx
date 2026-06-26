d5536b779bb75739c3880a7f07176e154cb8202a
Mukhammed <muhammatqudratov@gmail.com>
2026-06-26 14:56:53 +0900

    feat: add unversities correct location to the map logic

--- pages/university/detail.tsx
+++ pages/university/detail.tsx
@@ -445,7 +445,9 @@ const UniversityDetail: NextPage = ({ initialComment, ...props }: any) => {
 									<Typography className={'title'}>Address</Typography>
 									<Stack className={'map-box'}>
 										<iframe
-											src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25867.098915951767!2d128.68632810247993!3d35.86402299180927!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35660bba427bf179%3A0x1fc02da732b9072f!2sGeumhogangbyeon-ro%2C%20Dong-gu%2C%20Daegu!5e0!3m2!1suz!2skr!4v1695537640704!5m2!1suz!2skr"
+											src={`https://www.google.com/maps?q=${encodeURIComponent(
+												`${university?.universityAddress ?? ''}, ${university?.universityLocation ?? ''}`,
+											)}&output=embed`}
 											width="100%"
 											height="100%"
 											style={{ border: 0 }}
@@ -454,8 +456,8 @@ const UniversityDetail: NextPage = ({ initialComment, ...props }: any) => {
 											referrerPolicy="no-referrer-when-downgrade"
 										></iframe>
 										<Stack className={'map-tooltip'}>
-											<Typography className={'street'}>Geumhogangbyeon-ro</Typography>
-											<Typography className={'city'}>Dong-gu, Daegu</Typography>
+											<Typography className={'street'}>{university?.universityAddress}</Typography>
+											<Typography className={'city'}>{university?.universityLocation}</Typography>
 										</Stack>
 										<Box component={'div'} className={'map-pin'}>
 											<svg width="30" height="38" viewBox="0 0 30 38" fill="none" xmlns="http://www.w3.org/2000/svg">
